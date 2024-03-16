fbc6c4e5ba2218e5999fc70d7bb743b22cacefb7
Abhishek kumar <abhi.arya97161@gmail.com>
2024-03-16 15:15:23 +0530

    hero section completed

--- src/Page.jsx
+++ src/Page.jsx
@@ -5,7 +5,7 @@ const Page = () => {
   return (
     <div class="bg-slate-950 h-screen">
       {/* navbar starts here*/}
-      <div class="w-full h-14 bg-indigo-200 flex justify-between px-4 md:px-4 items-center">
+      <div class="w-full h-20 bg-indigo-300 flex justify-between px-4 md:px-4 items-center">
         <div class="text-indigo-800 font-bold text-2xl font-serif ">
           Welcome
         </div>
@@ -22,6 +22,22 @@ const Page = () => {
         </div>
       </div>
       {/* navbar ends here */}
+
+      {/* image section starts here  */}
+      <header class="w-full h-auto ">
+        <img
+          class="w-full hidden md:block"
+          src="https://images.pexels.com/photos/356056/pexels-photo-356056.jpeg"
+        />
+        <img
+          class="w-full md:hidden"
+          src="https://images.pexels.com/photos/2007647/pexels-photo-2007647.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+        />
+      </header>
+      {/* image section ends here */}
+
+        
+
     </div>
   );
 };
