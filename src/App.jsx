edd851277cfb6caf9aa75fb2958ecde382254e81
Abhishek kumar <abhi.arya97161@gmail.com>
2024-03-16 13:53:33 +0530

    created a card with small and large screens with text and color of fonts and also background color

--- src/App.jsx
+++ src/App.jsx
@@ -31,10 +31,40 @@ function App() {
             <p class="text-slate-500 text-center text-sm">Abhishek kumar</p>
           </div>
         </div>
-        <button class="bg-sky-500 text-white p-3 rounded-lg mt-3 hover:bg-white hover:text-black">Buy Now</button>
+        <button class="bg-sky-500 text-white p-3 rounded-lg mt-3 hover:bg-white hover:text-black">
+          Buy Now
+        </button>
 
-        <div class="text-center text-white my-2 text-xl">
-          <p class="text-white-400 sm:text-green-500 md:text-red-500">Lorem ipsum dolor sit</p>
+        {/* <div class="text-center text-white my-2 text-xl">
+          <p class="text-white-400 sm:text-green-500 md:text-red-500">
+            Lorem ipsum dolor sit
+          </p>
+        </div> */}
+
+        <div class="my-3">
+          <div class="max-w-sm mx-auto bg-white rounded-xl overflow-hidden md:max-w-lg">
+            <div class="md:flex">
+              <div class="">
+                <img
+                  class="h-48 w-full object-cover md:h-full md:w-88"
+                  src="https://images.pexels.com/photos/19820294/pexels-photo-19820294/free-photo-of-a-river-in-the-woods-with-trees-and-water.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                />
+              </div>
+              <div class="p-5">
+                <div class="uppercase text-center text-nowrap font-bold text-[15px] text-sky-600">
+                  Beautiful Nature
+                </div>
+                <a class="block mt-1 text-lg font-medium text-pretty text-center hover:text-sky-600" href="#">
+                  The nature is very beautiful and it also looks preety.
+                </a>
+                <p class="mt-1 text-sm text-balance text-center text-slate-500">
+                  <span class="bg-yellow-200 rounded-lg p-1 font-bold md:bg-yellow-800 md:text-black">Lorem ipsum</span>dolor sit amet consectetur adipisicing elit. Saepe
+                  tempora aliquam asperiores tempore harum laborum laboriosam
+                  aut reiciendis perspiciatis ducimus?
+                </p>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </>
